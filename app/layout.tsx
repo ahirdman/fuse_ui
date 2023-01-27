4200d9edeb4d618c42a787ee27cfc54e3fb397f5
Alexander Hirdman <hirdman.alex@icloud.com>
2023-01-27 16:42:31 +0100

    Style Sign in page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,9 @@
 import '../styles/global.css';
+import { Mulish } from '@next/font/google';
+
+const mulish = Mulish({
+  variable: '--font-mulish',
+});
 
 interface Props {
   children: React.ReactNode;
@@ -6,11 +11,7 @@ interface Props {
 
 export default function RootLayout({ children }: Props) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="en" className={`${mulish.variable} font-sans`}>
       <head />
       <body>{children}</body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,45 @@
 export default function Home() {
   return (
-    <main className="grid grid-rows-5 grid-cols-3 grid-flow-col justify-items-center h-screen w-screen bg-[#141416]">
-      <div className="p-10 text-center tracking-widest col-start-2 row-span-2 w-auto m-auto">
-        <h1 className="font-extrabold text-transparent tracking-widest text-9xl bg-clip-text bg-gradient-to-r from-orange-400 to-red-500">
-          FUSE
-        </h1>
+    <main className="grid grid-rows-5 grid-cols-1 md:grid-cols-5 2xl:grid-cols-3 grid-flow-col justify-items-center h-screen w-screen bg-[#141416]">
+      <div className="md:p-10 text-center tracking-widest md:col-start-3 2xl:col-start-2 row-span-2 flex flex-col justify-center">
+        <h1 className="font-extrabold text-9xl text-[#e75627]">FUSE</h1>
         <p className="text-white">Fuse your music library</p>
       </div>
-      <div className="text-zinc-300 bg-black rounded-xl px-10 col-start-2 row-start-3 row-span-2 flex flex-col justify-evenly">
-        <h1 className="text-lg text-center">Sign In</h1>
-        <Button label="Sign in with Email" />
-        <Button label="Sign in with Apple" />
-        <Button label="Sign in with Google" />
-        <p>
-          Dont have an account? <span className="font-bold">sign up</span> here
+      <form className="flex flex-col justify-self-stretch justify-evenly text-zinc-600 px-10 md:col-start-2 md:col-span-3 2xl:col-span-1 2xl:col-start-2 row-start-3 row-span-2">
+        <div className="flex flex-col">
+          <label htmlFor="email" className="pb-1">
+            Email address
+          </label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            className="text-white bg-transparent border-b border-zinc-700 pb-5 focus:outline-none hover:border-white caret-[#e75627]"
+          />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="password" className="pb-1">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="text-white bg-transparent border-b border-zinc-700 pb-5 focus:outline-none hover:border-white caret-[#e75627]"
+          />
+        </div>
+        <button
+          type="submit"
+          className="py-4 bg-zinc-800 text-zinc-500 rounded-3xl hover:text-white hover:shadow-lg focus:outline-none active:bg-[#e75627] transition duration-150 ease-in-out"
+        >
+          SIGN IN
+        </button>
+        <p className="text-center">
+          Dont have an account? <span className="text-white">Sign up</span>
         </p>
-      </div>
+      </form>
     </main>
   );
 }
 
-function Button({ label }: { label: string }) {
-  return <button className="py-1 px-6 text-white bg-black font-bold">{label}</button>;
-}
+// bg-transparent
