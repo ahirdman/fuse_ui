50f5348adb2b24ccc7e7685661ff33205c76e008
Alexander Hirdman <hirdman.alex@icloud.com>
2023-02-01 23:25:52 +0100

    Framer bug

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { FiCheck } from "react-icons/fi";
+import { LayoutGroup, usePresence } from "framer-motion";
 
 type User = true | undefined;
 
@@ -14,7 +15,6 @@ export default function SignUp() {
   const [password, setPassword] = useState("");
   // const [createUserWithEmailAndPassword, user, loading, error] =
   //   useCreateUserWithEmailAndPassword(auth);
-  const [spotifyAuthIsVisible, setSpotifyAuthIsVisible] = useState(false);
 
   const [user, setuser] = useState<User>(undefined);
 
@@ -30,123 +30,147 @@ export default function SignUp() {
   };
 
   return (
-    <form
+    <motion.form
+      layout
       onSubmit={handleSignUp}
-      className="row-span-2 row-start-3 flex flex-col justify-evenly justify-self-stretch px-10 text-zinc-600 md:col-span-3 md:col-start-2 2xl:col-span-1 2xl:col-start-2"
+      className={`row-span-2 row-start-3 flex flex-col justify-self-stretch overflow-hidden px-10 text-zinc-600 md:col-span-3 md:col-start-2 2xl:col-span-1 2xl:col-start-2 
+        `}
     >
-      <SignUpUpInput
-        setEmail={setEmail}
-        setPassword={setPassword}
-        user={user}
-      />
-      <div className="relative flex w-full items-center">
-        <SignUpButton user={user} />
-        <p
-          className={`absolute left-12  px-4 transition-opacity delay-500 duration-500 ${
-            user ? "z-0 text-white opacity-100" : "-z-10 opacity-0"
-          }`}
-        >
-          <span className="font-bold uppercase text-[#e75627]">Fuse</span>{" "}
-          account created
-        </p>
-      </div>
-
-      <SignInLink user={user} />
-      <AnimatePresence>
-        {spotifyAuthIsVisible && (
-          <motion.div
-            key="spotify"
-            className={`flex flex-col justify-between ${
-              user ? "opacity-100" : "opacity-0"
-            }`}
-          >
-            <p>
-              In order to use Fuse, you need to allow fuse to acces your spotify
-              library. Click the button below to authorize.
-            </p>
-            <button className="w-full rounded-2xl bg-zinc-800 p-4 uppercase text-zinc-500  hover:text-white hover:shadow-lg focus:outline-none">
-              Sign in to Spotify
-            </button>
-          </motion.div>
-        )}
+      <AnimatePresence mode="wait">
+        {!user ? (
+          <SignUpUpInput setEmail={setEmail} setPassword={setPassword} />
+        ) : null}
       </AnimatePresence>
-    </form>
+      <AnimatePresence mode="wait">
+        <motion.div
+          layout
+          transition={{ delay: 1.2, duration: 1 }}
+          className="relative flex w-full items-center"
+        >
+          <SignUpButton accountCreated={user} />
+          {user ? (
+            <motion.p
+              key="status"
+              layout
+              initial={{ opacity: 0, x: 100 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0 }}
+              transition={{ delay: 0.7, duration: 0.5 }}
+              className="  pl-4 text-white"
+            >
+              <span className="font-bold uppercase text-[#e75627]">Fuse</span>{" "}
+              account created
+            </motion.p>
+          ) : null}
+        </motion.div>
+      </AnimatePresence>
+      <AnimatePresence>
+        {!user ? <SignInLink /> : null}
+        {user ? <AuthorizeSpotify /> : null}
+      </AnimatePresence>
+    </motion.form>
   );
 }
 
 interface Props {
   setEmail: (value: string) => void;
   setPassword: (value: string) => void;
-  user: User;
 }
 
-const SignUpUpInput = ({ setEmail, setPassword, user }: Props) => {
+const SignUpUpInput = ({ setEmail, setPassword }: Props) => {
   return (
-    <div
-      className={`transition-all delay-500 duration-500 ${
-        user ? "h-0 opacity-0" : " h-40 opacity-100"
-      }`}
+    <motion.div
+      key="spotify"
+      layout
+      initial={false}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1.2 }}
+      className="flex flex-col pb-5"
     >
-      <div className="flex flex-col pb-5">
-        <label htmlFor="email" className="pb-1">
-          Email address
-        </label>
-        <input
-          type="text"
-          id="email"
-          name="email"
-          autoFocus
-          onChange={(e) => setEmail(e.target.value)}
-          className="border-b border-zinc-700 bg-transparent pb-4 text-white caret-[#e75627] hover:border-white focus:outline-none"
-        />
-      </div>
-      <div className="flex flex-col">
-        <label htmlFor="password" className="pb-1">
-          Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          onChange={(e) => setPassword(e.target.value)}
-          className="border-b border-zinc-700 bg-transparent pb-4 text-white caret-[#e75627] hover:border-white focus:outline-none"
-        />
-      </div>
-    </div>
+      <label htmlFor="email" className="pb-1">
+        Email address
+      </label>
+      <input
+        type="text"
+        id="email"
+        name="email"
+        autoFocus
+        onChange={(e) => setEmail(e.target.value)}
+        className="mb-2 border-b border-zinc-700 bg-transparent pb-4 text-white caret-[#e75627] hover:border-white focus:outline-none"
+      />
+      <label htmlFor="password" className="">
+        Password
+      </label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        onChange={(e) => setPassword(e.target.value)}
+        className="mb-2 border-b border-zinc-700 bg-transparent pb-4 text-white caret-[#e75627] hover:border-white focus:outline-none"
+      />
+    </motion.div>
   );
 };
 
-const SignInLink = ({ user }: { user: User }) => {
+const SignInLink = () => {
   return (
-    <p
-      className={`text-center transition-all duration-500 ease-in-out ${
-        user ? "opacity-0" : "opacity-100"
-      }`}
+    <motion.p
+      key="sign"
+      initial={{ opacity: 0, x: 100 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0 }}
+      className="text-center"
     >
       Already have an account?{" "}
       <Link href="/auth" className="text-white">
         Sign in
       </Link>
-    </p>
+    </motion.p>
   );
 };
 
-const SignUpButton = ({ user }: { user: User }) => {
+const SignUpButton = ({ accountCreated }: { accountCreated: User }) => {
   return (
-    <button
+    <motion.button
+      layout
+      key="hello"
+      initial={false}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
       type="submit"
-      disabled={user}
-      className={`h-12 rounded-2xl bg-zinc-800 uppercase text-zinc-500 transition-width duration-500 ease-out hover:text-white hover:shadow-lg focus:outline-none ${
-        user ? " w-12 " : "w-full"
+      disabled={accountCreated}
+      className={`h-12 rounded-2xl bg-zinc-800 uppercase text-zinc-500 transition-width duration-700 ease-out hover:text-white hover:shadow-lg focus:outline-none ${
+        accountCreated ? " w-12 " : "w-full"
       }`}
     >
-      {user ? (
+      {accountCreated ? (
         <span className="flex items-center justify-center text-white">
           <FiCheck />
         </span>
       ) : (
         "sign up"
       )}
-    </button>
+    </motion.button>
+  );
+};
+
+const AuthorizeSpotify = () => {
+  return (
+    <motion.div
+      key="about"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 3, delay: 1.5 }}
+      exit={{ opacity: 0.5 }}
+    >
+      <p className="mb-4">
+        You need to allow <span className="font-bold">fuse</span> to acces your
+        spotify library. Click the button below to authorize.
+      </p>
+      <button className="w-full rounded-2xl bg-zinc-800 p-4 uppercase text-zinc-500  hover:text-white hover:shadow-lg focus:outline-none">
+        Sign in to Spotify
+      </button>
+    </motion.div>
   );
 };
