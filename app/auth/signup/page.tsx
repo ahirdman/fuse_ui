6bf2fa8b6e372ed814103029ce703b6ed2296596
Alexander Hirdman <hirdman.alex@icloud.com>
2023-02-02 21:07:12 +0100

    Signup animation

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, Variants } from "framer-motion";
 import { auth } from "@/lib/firebase";
 import Link from "next/link";
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
@@ -33,41 +33,25 @@ export default function SignUp() {
     <motion.form
       layout
       onSubmit={handleSignUp}
-      className={`row-span-2 row-start-3 flex flex-col justify-self-stretch overflow-hidden px-10 text-zinc-600 md:col-span-3 md:col-start-2 2xl:col-span-1 2xl:col-start-2 
-        `}
+      className="row-span-2 row-start-3 flex flex-col justify-self-stretch overflow-hidden px-10 text-zinc-600 md:col-span-3 md:col-start-2 2xl:col-span-1 2xl:col-start-2"
     >
-      <AnimatePresence mode="wait">
+      <AnimatePresence>
         {!user ? (
           <SignUpUpInput setEmail={setEmail} setPassword={setPassword} />
         ) : null}
       </AnimatePresence>
-      <AnimatePresence mode="wait">
-        <motion.div
-          layout
-          transition={{ delay: 1.2, duration: 1 }}
-          className="relative flex w-full items-center"
-        >
-          <SignUpButton accountCreated={user} />
-          {user ? (
-            <motion.p
-              key="status"
-              layout
-              initial={{ opacity: 0, x: 100 }}
-              animate={{ opacity: 1, x: 0 }}
-              exit={{ opacity: 0 }}
-              transition={{ delay: 0.7, duration: 0.5 }}
-              className="  pl-4 text-white"
-            >
-              <span className="font-bold uppercase text-[#e75627]">Fuse</span>{" "}
-              account created
-            </motion.p>
-          ) : null}
-        </motion.div>
-      </AnimatePresence>
-      <AnimatePresence>
-        {!user ? <SignInLink /> : null}
-        {user ? <AuthorizeSpotify /> : null}
-      </AnimatePresence>
+      <motion.div
+        layout
+        transition={{ delay: 1.2, duration: 1 }}
+        className="flex w-full items-center justify-start"
+      >
+        <SignUpButton accountCreated={user} />
+        <AnimatePresence>
+          {user ? <AccountCreatedText /> : null}
+        </AnimatePresence>
+      </motion.div>
+      <AnimatePresence>{user ? null : <SignInLink />}</AnimatePresence>
+      <AnimatePresence>{user ? <AuthorizeSpotify /> : null}</AnimatePresence>
     </motion.form>
   );
 }
@@ -82,11 +66,14 @@ const SignUpUpInput = ({ setEmail, setPassword }: Props) => {
     <motion.div
       key="spotify"
       layout
-      initial={false}
-      animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 1.2 }}
-      className="flex flex-col pb-5"
+      initial={{ opacity: 0, x: 100 }}
+      animate={{ opacity: 1, height: "auto", x: 0 }}
+      exit={{
+        opacity: 0,
+        height: 0,
+        transition: { duration: 1, delay: 1.2 },
+      }}
+      className="flex flex-col overflow-hidden"
     >
       <label htmlFor="email" className="pb-1">
         Email address
@@ -95,7 +82,6 @@ const SignUpUpInput = ({ setEmail, setPassword }: Props) => {
         type="text"
         id="email"
         name="email"
-        autoFocus
         onChange={(e) => setEmail(e.target.value)}
         className="mb-2 border-b border-zinc-700 bg-transparent pb-4 text-white caret-[#e75627] hover:border-white focus:outline-none"
       />
@@ -113,36 +99,18 @@ const SignUpUpInput = ({ setEmail, setPassword }: Props) => {
   );
 };
 
-const SignInLink = () => {
-  return (
-    <motion.p
-      key="sign"
-      initial={{ opacity: 0, x: 100 }}
-      animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0 }}
-      className="text-center"
-    >
-      Already have an account?{" "}
-      <Link href="/auth" className="text-white">
-        Sign in
-      </Link>
-    </motion.p>
-  );
-};
-
 const SignUpButton = ({ accountCreated }: { accountCreated: User }) => {
   return (
     <motion.button
-      layout
-      key="hello"
-      initial={false}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
       type="submit"
+      layout
       disabled={accountCreated}
-      className={`h-12 rounded-2xl bg-zinc-800 uppercase text-zinc-500 transition-width duration-700 ease-out hover:text-white hover:shadow-lg focus:outline-none ${
-        accountCreated ? " w-12 " : "w-full"
-      }`}
+      initial={false}
+      animate={{
+        width: accountCreated ? "3rem" : "100%",
+      }}
+      transition={{ duration: 0.7, ease: "easeInOut" }}
+      className="my-4 h-12 rounded-2xl bg-zinc-800 uppercase text-zinc-500 hover:text-white hover:shadow-lg focus:outline-none"
     >
       {accountCreated ? (
         <span className="flex items-center justify-center text-white">
@@ -155,16 +123,50 @@ const SignUpButton = ({ accountCreated }: { accountCreated: User }) => {
   );
 };
 
+const AccountCreatedText = () => {
+  return (
+    <motion.p
+      key="status"
+      initial={{ opacity: 0, x: 100, width: 0 }}
+      animate={{ opacity: 1, x: 0, width: "auto" }}
+      exit={{ opacity: 0 }}
+      transition={{ delay: 0.7, duration: 0.5 }}
+      className="whitespace-nowrap pl-4 text-white"
+    >
+      <span className="font-bold uppercase text-[#e75627]">Fuse</span> account
+      created
+    </motion.p>
+  );
+};
+
+const SignInLink = () => {
+  return (
+    <motion.p
+      key="sign"
+      layout
+      initial={{ opacity: 0, x: 100 }}
+      animate={{ opacity: 1, height: 1, x: 0 }}
+      exit={{ opacity: 0, height: 0, transition: { duration: 1 } }}
+      className="text-center"
+    >
+      Already have an account?{" "}
+      <Link href="/auth" className="text-white">
+        Sign in
+      </Link>
+    </motion.p>
+  );
+};
+
 const AuthorizeSpotify = () => {
   return (
     <motion.div
       key="about"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ duration: 3, delay: 1.5 }}
-      exit={{ opacity: 0.5 }}
+      transition={{ duration: 1, delay: 1.2 }}
+      exit={{ opacity: 0 }}
     >
-      <p className="mb-4">
+      <p className="my-4">
         You need to allow <span className="font-bold">fuse</span> to acces your
         spotify library. Click the button below to authorize.
       </p>
