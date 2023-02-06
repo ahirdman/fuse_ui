3499c5a62161e3dea00a6dde9f4e0b18aeb483d1
Alexander Hirdman <hirdman.alex@icloud.com>
2023-02-06 20:53:59 +0100

    Fix button missplacement

--- app/auth/signup/spotify/page.tsx
+++ app/auth/signup/spotify/page.tsx
@@ -6,7 +6,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { FiCheck } from "react-icons/fi";
 
 export default function AuthenticateSpotify() {
-  const [user, loading, error] = useAuthState(auth);
+  const [error, loading, user] = useAuthState(auth);
 
   // Happy path:
   // Auth is done and redirected correclty
@@ -15,7 +15,7 @@ export default function AuthenticateSpotify() {
   // Something went wrong... ?
 
   return (
-    <div className="row-span-2 row-start-3 flex-col items-center justify-center justify-self-stretch overflow-hidden px-10 text-zinc-600 md:col-span-3 md:col-start-2 2xl:col-span-1 2xl:col-start-2">
+    <div className="row-span-2 row-start-3 items-center justify-center justify-self-stretch overflow-hidden px-10 text-zinc-600 md:col-span-3 md:col-start-2 2xl:col-span-1 2xl:col-start-2">
       <div className="flex items-center">
         <div className="my-4 h-12 w-12 rounded-2xl bg-zinc-800 uppercase text-zinc-500 hover:text-white hover:shadow-lg focus:outline-none">
           <span className="flex h-full items-center justify-center text-white">
@@ -33,13 +33,20 @@ export default function AuthenticateSpotify() {
             key="info"
             layout
             initial={false}
-            animate={{ opacity: 1, height: "auto" }}
+            animate={{
+              opacity: 1,
+              height: "auto",
+              marginTop: "1rem",
+              marginBottom: "1rem",
+            }}
             exit={{
               opacity: 0,
               height: 0,
+              marginTop: 0,
+              marginBottom: 0,
               transition: { duration: 0.5 },
             }}
-            className=" overflow-hidden text-center"
+            className="overflow-hidden text-center"
           >
             You need to allow <span className="font-bold">fuse</span> to acces
             your spotify library. Click the button below to authorize.
@@ -60,14 +67,14 @@ export default function AuthenticateSpotify() {
               width: user ? "3rem" : "100%",
             }}
             transition={{ delay: 0.5, duration: 1, ease: "easeInOut" }}
-            className="my-4 flex h-12  items-center justify-center rounded-2xl bg-zinc-800 uppercase text-zinc-500  "
+            className="flex h-12 items-center justify-center rounded-2xl bg-zinc-800 uppercase text-zinc-500"
           >
             {user ? (
               <span className="text-white">
                 <FiCheck />
               </span>
             ) : (
-              "Sign in with Spotify"
+              "Sign in to Spotify"
             )}
           </motion.button>
           {user ? (
