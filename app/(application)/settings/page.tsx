6f38da16b863b42f1915ae25530b8145c31b101b
Alexander Hirdman <hirdman.alex@icloud.com>
2023-02-07 16:07:34 +0100

    Loading screen as homepage and initial redirection

--- app/(application)/settings/page.tsx
+++ app/(application)/settings/page.tsx
@@ -1,17 +1,17 @@
-'use client';
+"use client";
 
-import { auth } from '@/lib/firebase';
-import { signOut } from 'firebase/auth';
-import { toast } from 'react-hot-toast';
+import { auth } from "@/lib/firebase";
+import { signOut } from "firebase/auth";
+import toast from "react-hot-toast";
 
 export default function Settings() {
   const handleSignOut = () => {
     signOut(auth);
-    toast.success('Signed out');
+    toast.success("Signed out");
   };
 
   return (
-    <div className="h-full grid">
+    <div className="grid h-full">
       <div className="place-self-center text-white">
         <button onClick={handleSignOut}>Sign Out</button>
       </div>
--- app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="grid h-full bg-red-700">
-      <div className="place-self-center text-white">Loading...</div>
-    </div>
-  );
-}
